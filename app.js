ffd3bce44000f739ccc6a2a6a5bf1581d1176dc7
joseluis.illana@beeva.com <joseluis.illana@beeva.com>
2015-11-26 12:53:04 +0100

    Versión estable para consultar a MongoDB

--- app.js
+++ app.js
@@ -4,11 +4,34 @@ var express         = require("express"),
     methodOverride  = require("method-override"),
     mongoose        = require('mongoose');
 
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
 // Connection to DB
 mongoose.connect('mongodb://10.211.55.112:27017/tvshows', function(err, res) {
-  if(err) throw err;
-  console.log('Connected to Database');
+  /*
+  Connection ready state
+    0 = disconnected
+    1 = connected
+    2 = connecting
+    3 = disconnecting
+  */
+  var connectionStatus = mongoose.connection.readyState;
+
+  if(err){
+    console.log("Error connecting to the database.");
+    throw err;
+  } else if (connectionStatus > 0 && connectionStatus < 3){
+    console.log('Connected to Database');
+  }else {
+    console.log("Not connected to the database");
+  }
 });
+
+// Favicon, Logging and cookies control
+//app.use(favicon());
+app.use(logger('dev'));
+app.use(cookieParser());
 
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,20 +50,32 @@ router.get('/', function(req, res) {
 app.use(router);
 
 // API routes
-var tvshows = express.Router();
+var routerTable = express.Router();
 
-tvshows.route('/tvshows')
+// Endpoints to ask mongodb
+routerTable.route('/tvshows')
   .get(TVShowCtrl.findAllTVShows)
   .post(TVShowCtrl.addTVShow);
 
-tvshows.route('/tvshows/:id')
+routerTable.route('/tvshows/:id')
   .get(TVShowCtrl.findById)
   .put(TVShowCtrl.updateTVShow)
   .delete(TVShowCtrl.deleteTVShow);
 
-app.use('/api', tvshows);
+// Endpoints to ask ElasticSearch
+
+
+// Add the routing table
+app.use('/api', routerTable);
 
 // Start server
 app.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
+  var connectionStatus = mongoose.connection.readyState;
+  if(connectionStatus > 1 && connectionStatus < 3){
+    console.log("M.I.K.E - Monitoring IT Key Engine\nAPI RESTful.");
+    console.log("WARNING - Running without connection to MongoDB.\nURL: http://localhost:3000");
+  } else {
+    console.log("M.I.K.E - Monitoring IT Key Engine\nAPI RESTful.");
+    console.log("Node server running.\nURL: http://localhost:3000");
+  }
 });
